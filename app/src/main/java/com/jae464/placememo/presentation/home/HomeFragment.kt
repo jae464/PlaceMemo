ef9880b274081339b3518464f75e84fa736a5770
이민재 <jae464@naver.com>
2022-07-11 01:40:23 +0900

    [FEAT] #3 Bottom Navigation View 구현

---
+++ app/src/main/java/com/jae464/placememo/presentation/home/HomeFragment.kt
@@ -0,0 +1,17 @@
+package com.jae464.placememo.presentation.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.jae464.placememo.R
+
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
