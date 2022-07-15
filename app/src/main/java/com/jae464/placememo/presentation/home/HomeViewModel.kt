e4d7ac737566b470437fc81ff480014cc13ccc95
이민재 <>
2022-07-16 03:17:15 +0900

    [FEAT] #6 지도 현 위치 이동 기능 추가

--- app/src/main/java/com/jae464/placememo/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/jae464/placememo/presentation/home/HomeViewModel.kt
@@ -1,6 +1,9 @@
 package com.jae464.placememo.presentation.home
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.naver.maps.map.util.FusedLocationSource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -8,5 +11,10 @@ import javax.inject.Inject
 class HomeViewModel @Inject constructor(
 
 ): ViewModel() {
+    private val _locationSource: MutableLiveData<FusedLocationSource> by lazy { MutableLiveData<FusedLocationSource>() }
+    val locationSource: LiveData<FusedLocationSource> = _locationSource
 
+    fun moveToUserLocation() {
+
+    }
 }
