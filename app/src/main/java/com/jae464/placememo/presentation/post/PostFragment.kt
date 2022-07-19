1ec63f93dd0763e4b10e25ee3085dd2879aff35f
이민재 <>
2022-07-20 02:18:27 +0900

    [FEAT] #8 글등록 화면 앱바 구현(Navigation 연결). 제목, 내용 UI 간단하게 만듬

--- app/src/main/java/com/jae464/placememo/presentation/post/PostFragment.kt
+++ app/src/main/java/com/jae464/placememo/presentation/post/PostFragment.kt
@@ -1,8 +1,13 @@
 package com.jae464.placememo.presentation.post
 
 import android.os.Bundle
+import android.view.Menu
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.jae464.placememo.R
 import com.jae464.placememo.presentation.base.BaseFragment
 import com.jae464.placememo.databinding.FragmentPostBinding
@@ -18,5 +23,25 @@ class PostFragment : BaseFragment<FragmentPostBinding>(R.layout.fragment_post) {
         val latitude = arguments?.getDouble("latitude")
         val longitude = arguments?.getDouble("longitude")
         println("$latitude, $longitude")
+        initAppBar()
     }
+
+    private fun initAppBar() {
+        var appBarConfiguration = AppBarConfiguration(findNavController().graph)
+        binding.postToolBar.setupWithNavController(findNavController(), appBarConfiguration)
+        binding.postToolBar.title = "게시글 업로드"
+        binding.postToolBar.inflateMenu(R.menu.post_toolbar_menu)
+        binding.postToolBar.setOnMenuItemClickListener {
+
+            // todo 게시글 저장
+            when(it.itemId) {
+                R.id.save -> {
+                    Toast.makeText(requireContext(),"저장버튼클릭", Toast.LENGTH_SHORT).show()
+                }
+            }
+            true
+        }
+
+    }
+
 }
