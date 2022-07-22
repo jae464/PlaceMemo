1ee2abf69934034300ec129d53bf4d78ba7bfab3
이민재 <>
2022-07-23 03:59:40 +0900

    [FEAT] #10 메모 저장, 조회를 위한 LocalDataSource 생성

---
+++ app/src/main/java/com/jae464/placememo/data/repository/post/local/MemoLocalDataSource.kt
@@ -0,0 +1,9 @@
+package com.jae464.placememo.data.repository.post.local
+
+import com.jae464.placememo.data.model.MemoEntity
+import com.jae464.placememo.domain.model.post.Memo
+
+interface MemoLocalDataSource {
+    suspend fun getMemo(id: Long): MemoEntity
+    suspend fun saveMemo(memo: MemoEntity)
+}
---
+++ app/src/main/java/com/jae464/placememo/data/repository/post/local/MemoLocalDataSourceImpl.kt
@@ -0,0 +1,18 @@
+package com.jae464.placememo.data.repository.post.local
+
+import com.jae464.placememo.data.db.MemoDao
+import com.jae464.placememo.data.model.MemoEntity
+import com.jae464.placememo.domain.model.post.Memo
+import javax.inject.Inject
+
+class MemoLocalDataSourceImpl @Inject constructor(
+    private val memoDao: MemoDao
+): MemoLocalDataSource {
+    override suspend fun getMemo(id: Long): MemoEntity {
+        return memoDao.getMemo(id)
+    }
+
+    override suspend fun saveMemo(memo: MemoEntity) {
+        memoDao.insertMemo(memo)
+    }
+}
