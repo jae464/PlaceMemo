d8d7bc27552890557fefeb124c15a4199f1580d8
이민재 <jae464@naver.com>
2022-08-01 00:17:17 +0900

    [FEAT] #10 이미지 업로드 (아직 하나의 이미지밖에 안된다) 구현.

--- app/src/main/java/com/jae464/placememo/domain/model/post/Memo.kt
+++ app/src/main/java/com/jae464/placememo/domain/model/post/Memo.kt
@@ -1,12 +1,14 @@
 package com.jae464.placememo.domain.model.post
 
+import android.graphics.Bitmap
+import android.net.Uri
 import java.util.*
 
 data class Memo (
     val id: Long,
     val title: String,
     val content: String,
-    val imageUrlList: List<String>,
+    val imageUrlList: Bitmap?,
     val latitude: Double,
     val longitude: Double
 )
--- app/src/main/java/com/jae464/placememo/presentation/post/PostViewModel.kt
+++ app/src/main/java/com/jae464/placememo/presentation/post/PostViewModel.kt
@@ -1,5 +1,7 @@
 package com.jae464.placememo.presentation.post
 
+import android.graphics.Bitmap
+import android.net.Uri
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -14,9 +16,21 @@ import javax.inject.Inject
 class PostViewModel @Inject constructor(
     private val repository: MemoRepository
 ) : ViewModel() {
-    fun saveMemo(memo: Memo) {
+
+    private val _imageList = MutableLiveData<Bitmap>()
+    val imageList: LiveData<Bitmap> get() = _imageList
+
+    fun saveMemo(id: Long, title: String, content: String, latitude: Double, longitude: Double) {
         viewModelScope.launch {
+            val memo = Memo(id, title, content, imageList.value, latitude, longitude)
+            println("저장 전 내용 확인합니다.")
+            println("$id $title $content $latitude $longitude")
+            println(imageList)
             repository.saveMemo(memo)
         }
+    }
+
+    fun setImageList(image: Bitmap) {
+        _imageList.postValue(image)
     }
 }
