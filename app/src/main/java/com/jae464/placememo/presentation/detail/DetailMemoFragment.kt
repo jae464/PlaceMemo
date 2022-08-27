b04469235a5387b51a58ea6e17abc9c4241b1366
이민재 <>
2022-08-27 17:51:28 +0900

    [FEAT] #19 레포지토리에서 해당 메모 불러오기 구현

--- app/src/main/java/com/jae464/placememo/presentation/detail/DetailMemoFragment.kt
+++ app/src/main/java/com/jae464/placememo/presentation/detail/DetailMemoFragment.kt
@@ -3,6 +3,7 @@ package com.jae464.placememo.presentation.detail
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.fragment.app.viewModels
 import androidx.navigation.NavArgs
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
@@ -11,20 +12,33 @@ import androidx.navigation.ui.setupWithNavController
 import com.jae464.placememo.R
 import com.jae464.placememo.databinding.FragmentDetailMemoBinding
 import com.jae464.placememo.presentation.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class DetailMemoFragment: BaseFragment<FragmentDetailMemoBinding>(R.layout.fragment_detail_memo) {
 
     private val args: DetailMemoFragmentArgs by navArgs()
+    private val viewModel: DetailMemoViewModel by viewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         Log.d("DetailMemoFragment", "view created")
+        binding.viewModel = viewModel
+        viewModel.getMemo(args.memoId)
         initAppBar()
+        initObserver()
     }
 
     private fun initAppBar() {
         val appBarConfiguration = AppBarConfiguration(findNavController().graph)
-        binding.detailToolBar.setupWithNavController(findNavController(), appBarConfiguration)
-        binding.detailToolBar.title = args.memoId.toString()
-        binding.detailToolBar.inflateMenu(R.menu.detail_toolbar_menu)
+        binding.detailToolbar.setupWithNavController(findNavController(), appBarConfiguration)
+        binding.detailToolbar.title = args.memoId.toString()
+        binding.detailToolbar.inflateMenu(R.menu.detail_toolbar_menu)
+    }
+
+    private fun initObserver() {
+        viewModel.memo.observe(viewLifecycleOwner) {
+
+        }
     }
 }
