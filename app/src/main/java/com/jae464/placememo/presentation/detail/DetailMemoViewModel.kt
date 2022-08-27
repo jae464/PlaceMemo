dc662bcceeb31e457aded4e99645818d1fe0125f
이민재 <jae464@naver.com>
2022-08-27 17:51:28 +0900

    [FEAT] #19 레포지토리에서 해당 메모 불러오기 구현

--- app/src/main/java/com/jae464/placememo/presentation/detail/DetailMemoViewModel.kt
+++ app/src/main/java/com/jae464/placememo/presentation/detail/DetailMemoViewModel.kt
@@ -1,8 +1,26 @@
 package com.jae464.placememo.presentation.detail
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.jae464.placememo.domain.model.post.Memo
+import com.jae464.placememo.domain.repository.MemoRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class DetailMemoViewModel: ViewModel() {
+@HiltViewModel
+class DetailMemoViewModel @Inject constructor(
+    private val memoRepository: MemoRepository
+): ViewModel() {
 
+    private val _memo = MutableLiveData<Memo>()
+    val memo: LiveData<Memo> get() = _memo
+
+    fun getMemo(id: Long) {
+        viewModelScope.launch {
+           _memo.postValue(memoRepository.getMemo(id))
+        }
+    }
 }
