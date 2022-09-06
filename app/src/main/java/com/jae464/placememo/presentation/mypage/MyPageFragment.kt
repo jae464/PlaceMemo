6ccd7930ce4c77fba05a69f48a49869eea596c50
이민재 <jae464@naver.com>
2022-09-07 02:30:44 +0900

    [FEAT] #20 구글, 파이어베이스 로그인 로그아웃 테스트

--- app/src/main/java/com/jae464/placememo/presentation/mypage/MyPageFragment.kt
+++ app/src/main/java/com/jae464/placememo/presentation/mypage/MyPageFragment.kt
@@ -6,13 +6,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.jae464.placememo.LoginActivity
+import com.jae464.placememo.MainActivity
 import com.jae464.placememo.R
+import com.jae464.placememo.databinding.FragmentMyPageBinding
+import com.jae464.placememo.presentation.base.BaseFragment
 
-class MyPageFragment : Fragment(R.layout.fragment_my_page) {
+class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val intent = Intent(requireContext(), LoginActivity::class.java)
-        startActivity(intent)
+        initListener()
+        val user = FirebaseAuth.getInstance().currentUser
+        // 로그인 상태가 아닐 때 로그인 화면으로 이동
+        if (user == null) {
+            val intent = Intent(requireContext(), LoginActivity::class.java)
+            startActivity(intent)
+        }
+        binding.userEmailTextView.text = user?.email
+    }
+
+    private fun initListener() {
+        binding.logoutButton.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            Toast.makeText(context, "로그아웃 완료", Toast.LENGTH_SHORT).show()
+            val intent = Intent(requireContext(), MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
