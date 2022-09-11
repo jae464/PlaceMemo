00372ffbfa978a45bbbbdd48f420af389cabc3ab
이민재 <>
2022-09-12 00:31:43 +0900

    [FEAT] #23 최초 로그인시 닉네임 설정을 위한 다이얼로그 기본 틀 구현

---
+++ app/src/main/java/com/jae464/placememo/data/repository/login/remote/LoginRemoteDataSource.kt
@@ -0,0 +1,4 @@
+package com.jae464.placememo.data.repository.login.remote
+
+interface LoginRemoteDataSource {
+}
---
+++ app/src/main/java/com/jae464/placememo/data/repository/login/remote/LoginRemoteDataSourceImpl.kt
@@ -0,0 +1,4 @@
+package com.jae464.placememo.data.repository.login.remote
+
+class LoginRemoteDataSourceImpl {
+}
