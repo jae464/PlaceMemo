7056e3813e9314a008e951717cdaa2b81f89e206
이민재 <jae464@naver.com>
2022-09-12 02:44:16 +0900

    [FIX] coroutine dependency 추가.

--- app/src/main/java/com/jae464/placememo/data/repository/login/remote/LoginRemoteDataSourceImpl.kt
+++ app/src/main/java/com/jae464/placememo/data/repository/login/remote/LoginRemoteDataSourceImpl.kt
@@ -4,23 +4,34 @@ import android.util.Log
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.jae464.placememo.data.model.UserEntity
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.tasks.await
+import okhttp3.internal.wait
 
-class LoginRemoteDataSourceImpl: LoginRemoteDataSource {
+class LoginRemoteDataSourceImpl : LoginRemoteDataSource {
     private val firestore = Firebase.firestore
 
     override suspend fun getUserInfoWithUid(uid: String) {
+        var userInfo: UserEntity? = null
         val userRef = firestore.collection("users")
             .document(uid)
 
-        var userInfo: UserEntity? = null
         userRef.get()
             .addOnSuccessListener {
                 Log.d("LoginRemoteDataSourceImpl", it.data.toString())
-                userInfo?.uid = it.data?.get("uid").toString()
-                userInfo?.email = it.data?.get("email").toString()
+                userInfo = UserEntity(it.data?.get("uid").toString(),
+                it.data?.get("email").toString())
             }
+            .addOnFailureListener {
 
-        Log.d("LoginRemoteDataSourceImpl", userInfo.toString())
+            }
+            .await()
+
+
+        Log.d("LoginRemoteDataSourceImpl", "$userInfo")
+
     }
 
     override suspend fun setUserInfo(user: UserEntity) {
