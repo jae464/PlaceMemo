d520a86438b74fe88a5c0d103331af2b6ce0c73f
이민재 <jae464@naver.com>
2022-09-12 15:20:39 +0900

    [FEAT] #23 로그인 및 유저 파이어스토어 저장 기능 뷰모델로 이동

--- app/src/main/java/com/jae464/placememo/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/jae464/placememo/presentation/login/LoginViewModel.kt
@@ -1,8 +1,11 @@
 package com.jae464.placememo.presentation.login
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.jae464.placememo.domain.model.login.User
 import com.jae464.placememo.domain.repository.LoginRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -12,12 +15,21 @@ import javax.inject.Inject
 class LoginViewModel @Inject constructor(
     private val loginRepository: LoginRepository
 ): ViewModel() {
+    private val _user = MutableLiveData<User?>()
+    val user: LiveData<User?> get() = _user
+
     fun getUserInfo(uid: String) {
-
         viewModelScope.launch {
-            val result = loginRepository.getUserInfo(uid)
-            Log.d("LoginViewModel", result.toString())
+            val userInfo = loginRepository.getUserInfo(uid)
+            Log.d("LoginViewModel", userInfo.toString())
+            _user.postValue(userInfo)
         }
+    }
 
+    fun setUserInfo(user: User) {
+        viewModelScope.launch {
+            loginRepository.setUserInfo(user)
+            Log.d("LoginViewModel", "setUser 완료")
+        }
     }
 }
