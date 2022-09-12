461c07a2111119a8eecdfd7d0c717a32b40d5e03
이민재 <>
2022-09-12 15:04:50 +0900

    [FEAT] 로그인 및 파이어스토어 저장 기능 뷰모델에 구현

--- app/src/main/java/com/jae464/placememo/data/repository/login/LoginRepositoryImpl.kt
+++ app/src/main/java/com/jae464/placememo/data/repository/login/LoginRepositoryImpl.kt
@@ -1,21 +1,24 @@
 package com.jae464.placememo.data.repository.login
 
+import com.jae464.placememo.data.mapper.userEntityToUser
+import com.jae464.placememo.data.mapper.userToUserEntity
 import com.jae464.placememo.data.repository.login.remote.LoginRemoteDataSource
+import com.jae464.placememo.domain.model.login.User
 import com.jae464.placememo.domain.repository.LoginRepository
 import javax.inject.Inject
 
 class LoginRepositoryImpl @Inject constructor(
     private val loginRemoteDataSource: LoginRemoteDataSource
 ): LoginRepository {
-    override fun set() {
-        TODO("Not yet implemented")
+    override suspend fun setUserInfo(user: User) {
+        loginRemoteDataSource.setUserInfo(userToUserEntity(user))
     }
 
     override fun doLogout() {
         TODO("Not yet implemented")
     }
 
-    override suspend fun getUserInfo(uid: String) {
-        loginRemoteDataSource.getUserInfoWithUid(uid)
+    override suspend fun getUserInfo(uid: String): User? {
+        return userEntityToUser(loginRemoteDataSource.getUserInfoWithUid(uid))
     }
 }
--- app/src/main/java/com/jae464/placememo/domain/repository/LoginRepository.kt
+++ app/src/main/java/com/jae464/placememo/domain/repository/LoginRepository.kt
@@ -1,7 +1,9 @@
 package com.jae464.placememo.domain.repository
 
+import com.jae464.placememo.domain.model.login.User
+
 interface LoginRepository {
-    fun set()
+    suspend fun setUserInfo(user: User)
     fun doLogout()
-    suspend fun getUserInfo(uid: String)
+    suspend fun getUserInfo(uid: String): User?
 }
