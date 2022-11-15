e3aea4732a0bb727dc823e862f52a2c80ee4404a
이민재 <>
2022-11-16 01:29:28 +0900

    [FEAT] 마이페이지 user 닉네임, 이메일 표시 구현. 프로필 이미지 모양 수정.

--- app/src/main/java/com/jae464/placememo/presentation/mypage/MyPageFragment.kt
+++ app/src/main/java/com/jae464/placememo/presentation/mypage/MyPageFragment.kt
@@ -4,34 +4,44 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.google.firebase.auth.FirebaseAuth
 import com.jae464.placememo.presentation.login.LoginActivity
 import com.jae464.placememo.MainActivity
 import com.jae464.placememo.R
 import com.jae464.placememo.databinding.FragmentMyPageBinding
 import com.jae464.placememo.presentation.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
 
-    private val user = FirebaseAuth.getInstance().currentUser
+    private val firebaseUser = FirebaseAuth.getInstance().currentUser
+    private val viewModel: MyPageViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = viewModel
         initListener()
         initView()
-
+        initObserver()
+        initAppBar()
     }
 
     private fun initView() {
-        Log.d("MyPageFragment", user?.email.toString())
-        if (user == null) {
+        Log.d("MyPageFragment", firebaseUser?.email.toString())
+        if (firebaseUser == null) {
             binding.loginViewGroup.visibility = View.GONE
             binding.logoutViewGroup.visibility = View.VISIBLE
         }
         else {
             binding.logoutViewGroup.visibility = View.GONE
             binding.loginViewGroup.visibility = View.VISIBLE
-            binding.nickNameTextView.text = user.email
+            binding.userProfileImageView.clipToOutline = true
+            viewModel.getUser(firebaseUser.uid)
         }
     }
 
@@ -47,6 +57,15 @@ class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_
             val intent = Intent(context, LoginActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun initAppBar() {
+        val appBarConfiguration = AppBarConfiguration(findNavController().graph)
+        binding.myPageToolBar.setupWithNavController(findNavController(), appBarConfiguration)
+
+    }
+
+    private fun initObserver() {
 
     }
 }
