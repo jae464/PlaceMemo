a371dcb817851c3f82fbd077edd211beca68c34d
이민재 <jae464@naver.com>
2022-11-16 01:29:28 +0900

    [FEAT] 마이페이지 user 닉네임, 이메일 표시 구현. 프로필 이미지 모양 수정.

---
+++ app/src/main/java/com/jae464/placememo/presentation/mypage/MyPageViewModel.kt
@@ -0,0 +1,29 @@
+package com.jae464.placememo.presentation.mypage
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.jae464.placememo.domain.model.login.User
+import com.jae464.placememo.domain.repository.LoginRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MyPageViewModel @Inject constructor(
+    private val loginRepository: LoginRepository
+): ViewModel() {
+    private val _user = MutableLiveData<User?>()
+    val user: LiveData<User?> get() = _user
+
+    fun getUser(uid: String) {
+        viewModelScope.launch {
+            val user = loginRepository.getUserInfo(uid)
+            Log.d("MyPageViewModel", user.toString())
+            _user.postValue(user)
+        }
+    }
+
+}
