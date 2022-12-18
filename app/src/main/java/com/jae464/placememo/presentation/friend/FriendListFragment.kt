92dfea9c40c616560b5463c2edabe3381af21830
이민재 <>
2022-12-19 01:33:55 +0900

    [FEAT] 친구 목록 구현을 위한 FriendListFragment 생성. 리사이클러뷰 구현을 위해 item_friend_list 파일 생성

---
+++ app/src/main/java/com/jae464/placememo/presentation/friend/FriendListFragment.kt
@@ -0,0 +1,20 @@
+package com.jae464.placememo.presentation.friend
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.jae464.placememo.R
+import com.jae464.placememo.databinding.FragmentFriendListBinding
+import com.jae464.placememo.presentation.base.BaseFragment
+
+class FriendListFragment : BaseFragment<FragmentFriendListBinding>(R.layout.fragment_friend_list) {
+
+    val TAG = "FreindListFragment"
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
