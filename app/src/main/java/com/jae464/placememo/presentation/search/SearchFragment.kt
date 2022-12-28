5512b1add13b43244e68bd0e72e89300bea8f8d2
이민재 <>
2022-12-29 01:07:51 +0900

    [FEAT] 검색창 구현

--- app/src/main/java/com/jae464/placememo/presentation/search/SearchFragment.kt
+++ app/src/main/java/com/jae464/placememo/presentation/search/SearchFragment.kt
@@ -1,5 +1,10 @@
 package com.jae464.placememo.presentation.search
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import com.jae464.placememo.R
 import com.jae464.placememo.databinding.FragmentSearchBinding
 import com.jae464.placememo.presentation.base.BaseFragment
@@ -8,4 +13,17 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
 
+    private val viewModel: SearchViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = viewModel
+
+        initAppBar()
+    }
+
+    private fun initAppBar() {
+        val appBarConfiguration = AppBarConfiguration(findNavController().graph)
+
+    }
 }
