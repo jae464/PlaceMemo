4df33f40218555b573025d6020b758676192dff4
이민재 <jae464@naver.com>
2022-12-31 03:12:57 +0900

    [FEAT] EditText 입력 중 다른 화면 클릭 시 키보드 내리도록 구현

--- app/src/main/java/com/jae464/placememo/MainActivity.kt
+++ app/src/main/java/com/jae464/placememo/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.jae464.placememo
 
+import android.content.Context
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.view.isVisible
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -32,5 +37,25 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
             binding.bottomNavigationView.isVisible =
                 (arguments?.getBoolean("ShowBottomNavigationView", true) ?: true) == true
         }
+    }
+
+    // EditText 에서 키보드 올라왔을때, 다른 화면 클릭 시 키보드 내리는 메소드
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        val view = currentFocus
+
+        if (view != null && (ev?.action == MotionEvent.ACTION_UP || ev?.action == MotionEvent.ACTION_MOVE) && view is EditText) {
+            val scrcoords = IntArray(2)
+
+            view.getLocationOnScreen(scrcoords)
+
+            val x = ev.rawX + view.left - scrcoords[0]
+            val y = ev.rawY + view.top - scrcoords[1]
+
+            if (x < view.left || x > view.right || y < view.top || y > view.bottom) {
+                (this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager).hideSoftInputFromWindow(
+                    (this.window.decorView.applicationWindowToken), 0)
+            }
+        }
+        return super.dispatchTouchEvent(ev)
     }
 }
