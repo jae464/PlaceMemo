1616838ec676739ee285ace0be6fa722458dd6c5
이민재 <jae464@naver.com>
2023-01-24 00:40:49 +0900

    [FEAT] 친구 리스트 화면 구현 (툴바) 및 툴바 커스텀 테마 생성 및 적용

---
+++ app/src/main/java/com/jae464/placememo/presentation/friend/FreindViewModel.kt
@@ -0,0 +1,12 @@
+package com.jae464.placememo.presentation.friend
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class FreindViewModel @Inject constructor(
+
+) : ViewModel() {
+
+}
--- app/src/main/java/com/jae464/placememo/presentation/friend/FriendListFragment.kt
+++ app/src/main/java/com/jae464/placememo/presentation/friend/FriendListFragment.kt
@@ -6,15 +6,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.jae464.placememo.R
 import com.jae464.placememo.databinding.FragmentFriendListBinding
 import com.jae464.placememo.presentation.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class FriendListFragment : BaseFragment<FragmentFriendListBinding>(R.layout.fragment_friend_list) {
 
     val TAG = "FreindListFragment"
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initAppBar()
+
+    }
+
+    private fun initAppBar() {
+        val appBarConfiguration = AppBarConfiguration(findNavController().graph)
+        binding.friendListToolbar.setupWithNavController(findNavController(), appBarConfiguration)
+        binding.friendListToolbar.inflateMenu(R.menu.friend_list_toolbar_menu)
     }
 }
