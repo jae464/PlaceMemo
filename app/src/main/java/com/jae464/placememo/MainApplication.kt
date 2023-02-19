019010527f1f520f6f25b5a0241128fda7c24d6b
이민재 <jae464@naver.com>
2023-02-20 00:48:56 +0900

    [FIX] app 모듈 dependencies 수정

--- app/src/main/java/com/jae464/placememo/MainApplication.kt
+++ app/src/main/java/com/jae464/placememo/MainApplication.kt
@@ -1,7 +1,6 @@
 package com.jae464.placememo
 
 import android.app.Application
-import com.naver.maps.map.NaverMapSdk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -9,7 +8,7 @@ class MainApplication: Application() {
     override fun onCreate() {
         super.onCreate()
         // Naver Map 불러오기
-        NaverMapSdk.getInstance(this).client =
-            NaverMapSdk.NaverCloudPlatformClient(BuildConfig.NAVER_API_KEY)
+//        NaverMapSdk.getInstance(this).client =
+//            NaverMapSdk.NaverCloudPlatformClient(BuildConfig.NAVER_API_KEY)
     }
 }
