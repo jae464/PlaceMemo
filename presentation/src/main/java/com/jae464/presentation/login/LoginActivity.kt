b38a337d11ca1910390ac3206163a40a1e7fc810
10152954 <minjae12.lee@kt.com>
2023-02-22 13:33:26 +0900

    [FIX] 구글로그인 인텐트 실행 방식 변경 (registerForActivityResult 사용)

--- presentation/src/main/java/com/jae464/presentation/login/LoginActivity.kt
+++ presentation/src/main/java/com/jae464/presentation/login/LoginActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.registerForActivityResult
 import androidx.activity.viewModels
 import androidx.fragment.app.DialogFragment
 import com.google.android.gms.auth.api.signin.GoogleSignIn
@@ -27,6 +29,22 @@ SettingNicknameDialog.NoticeDialogListener {
     private lateinit var auth: FirebaseAuth
     private val viewModel: LoginViewModel by viewModels()
 
+    private val googleLoginLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
+        var account: GoogleSignInAccount? = null
+        try {
+            account = task.getResult(ApiException::class.java)
+            Log.d("LoginActivity", account.toString())
+            firebaseAuthWithGoogle(account!!.idToken)
+            Toast.makeText(this, "sucess google login", Toast.LENGTH_SHORT).show()
+//                goToMain()
+        } catch (e: ApiException) {
+            Toast.makeText(this, "Failed Google Login", Toast.LENGTH_SHORT).show()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         auth = FirebaseAuth.getInstance()
@@ -44,7 +62,7 @@ SettingNicknameDialog.NoticeDialogListener {
 
     private fun initListener() {
         binding.loginButton.setOnClickListener {
-            startActivityForResult(client.signInIntent, 1)
+            googleLoginLauncher.launch(client.signInIntent)
         }
 
         binding.dialogButton.setOnClickListener {
@@ -53,28 +71,10 @@ SettingNicknameDialog.NoticeDialogListener {
 
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == 1) {
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            var account: GoogleSignInAccount? = null
-            try {
-                account = task.getResult(ApiException::class.java)
-                Log.d("LoginActivity", account.toString())
-                firebaseAuthWithGoogle(account!!.idToken)
-                Toast.makeText(this, "sucess google login", Toast.LENGTH_SHORT).show()
-//                goToMain()
-            } catch (e: ApiException) {
-                Toast.makeText(this, "Failed Google Login", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-
     private fun firebaseAuthWithGoogle(idToken: String?) {
         val credential = GoogleAuthProvider.getCredential(idToken, null)
         auth.signInWithCredential(credential)
-            .addOnCompleteListener(this
-            ) { task ->
+            .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     // 인증에 성공한 후, 현재 로그인된 유저의 정보를 가져올 수 있습니다.
                     val email = auth.currentUser?.email
