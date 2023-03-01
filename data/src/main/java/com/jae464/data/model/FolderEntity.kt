9ba99c464ae58a8198fa66310e4f585dbc09578a
이민재 <jae464@naver.com>
2023-03-01 18:10:52 +0900

    [FEAT] 기존 FeedFragmet를 FeedFolderFragment, FeedCategoryFragment로 분리. FeedFragment에서 (TabLayout, ViewPager2 사용)

--- data/src/main/java/com/jae464/data/model/FolderEntity.kt
+++ data/src/main/java/com/jae464/data/model/FolderEntity.kt
@@ -3,7 +3,6 @@ package com.jae464.data.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.google.gson.annotations.SerializedName
 
 @Entity(tableName = "folder")
 data class FolderEntity(
---
+++ presentation/src/main/java/com/jae464/presentation/feed/FeedFolderFragment.kt
@@ -0,0 +1,10 @@
+package com.jae464.presentation.feed
+
+import com.jae464.presentation.base.BaseFragment
+import com.jae464.presentation.R
+import com.jae464.presentation.databinding.FragmentFeedFolderBinding
+
+class FeedFolderFragment : BaseFragment<FragmentFeedFolderBinding>(R.layout.fragment_feed_folder) {
+
+
+}
