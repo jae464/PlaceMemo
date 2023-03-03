b7f29604d8c33e449f9845eb213a820755872326
이민재 <jae464@naver.com>
2023-03-04 01:38:29 +0900

    [FEAT] 로고 변경. LocationManager 에서 FusedLocationProviderClient 로 변경. 주소 API 예외처리.

--- presentation/src/main/java/com/jae464/presentation/base/BaseMapFragment.kt
+++ presentation/src/main/java/com/jae464/presentation/base/BaseMapFragment.kt
@@ -8,43 +8,61 @@ import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Looper
 import android.provider.Settings
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.LayoutRes
+import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.material.snackbar.Snackbar
 import com.jae464.presentation.R
 
-abstract class BaseMapFragment<T: ViewDataBinding>(@LayoutRes val layoutRes: Int): Fragment(), OnMapReadyCallback {
+abstract class BaseMapFragment<T : ViewDataBinding>(@LayoutRes val layoutRes: Int) : Fragment(),
+    OnMapReadyCallback {
     private var _binding: T? = null
     protected val binding get() = _binding!!
 
-    private lateinit var  locationManager: LocationManager
+    private lateinit var locationManager: LocationManager
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private val locationRequest =
+        LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 1000L).build()
+    private lateinit var locationCallback: LocationCallback
     lateinit var map: GoogleMap
     private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        println("BaseFragment onCreate")
-        // 권한 요청하기
-        requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
-                isGranted: Boolean ->
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(activity!!.applicationContext)
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                Log.d("BaseMapFragment", locationResult.toString())
+            }
+        }
+        requestPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted ->
             if (isGranted) {
-                setUserLocation()
+                setCurrentLocation()
             }
             else {
-                showPermissionSnackBar(binding.root)
+                Toast.makeText(requireContext(), "현위치 사용을 위해서는 권한 설정이 필요합니다.", Toast.LENGTH_SHORT).show()
+                return@registerForActivityResult
             }
         }
     }
@@ -59,6 +77,18 @@ abstract class BaseMapFragment<T: ViewDataBinding>(@LayoutRes val layoutRes: Int
         (childFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment)
             .getMapAsync(this)
         locationManager = context?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        // 에뮬레이터용 초기위치 업데이트
+        checkPermission()
+
+        locationManager.requestLocationUpdates(
+            LocationManager.GPS_PROVIDER,
+            100,
+            5f
+        ) { location ->
+            Log.d("BaseMapFragment", location.toString())
+        }
+
         return binding.root
     }
 
@@ -68,58 +98,39 @@ abstract class BaseMapFragment<T: ViewDataBinding>(@LayoutRes val layoutRes: Int
         map.uiSettings.isMapToolbarEnabled = false
     }
 
-    fun setUserLocation() {
-        if(ActivityCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            // 권한 설정 필요
-            requestPermissionLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
-        }
-        else {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 100, 5f, object: LocationListener {
-                override fun onLocationChanged(location: Location) {
-                    map.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(location.latitude, location.longitude),16f), 1000, null)
-                }
-
-            })
-            return
-        }
-    }
 
     private fun showPermissionSnackBar(layout: View) {
         Snackbar.make(layout, "위치 권한 설정이 필요합니다.", Snackbar.LENGTH_INDEFINITE).apply {
             setAction("권한 설정") {
                 val intent = Intent(
                     Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
-                    Uri.fromParts("package", context.packageName, null))
+                    Uri.fromParts("package", context.packageName, null)
+                )
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                 startActivity(intent)
             }.show()
         }
     }
 
-    fun getLocation(): Location? {
-        if (ActivityCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            return null
-        }
+    fun getLocation() {
 
-        return locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+        checkPermission()
+//        return locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location ->
+                Log.d("BaseMapFragment", location.toString())
+                map.animateCamera(
+                    CameraUpdateFactory.newLatLngZoom(
+                        LatLng(
+                            location.latitude,
+                            location.longitude
+                        ), 16f
+                    ), 1000, null
+                )
+            }
     }
 
     fun setCurrentLocation() {
-        val locationListener = LocationListener {location ->
-            Log.d("BaseMapFragment", "Location Changed to $location")
-            map.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(location.latitude, location.longitude),16f), 1000, null)
-        }
 
         if (ActivityCompat.checkSelfPermission(
                 requireContext(),
@@ -129,11 +140,51 @@ abstract class BaseMapFragment<T: ViewDataBinding>(@LayoutRes val layoutRes: Int
                 Manifest.permission.ACCESS_COARSE_LOCATION
             ) != PackageManager.PERMISSION_GRANTED
         ) {
+            map.animateCamera(
+                CameraUpdateFactory.newLatLngZoom(
+                    LatLng(
+                        37.554891, 126.970814
+                    ), 16f
+                ), 1000, null
+            )
             return
         }
 
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 100, 5f, locationListener)
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location ->
+                Log.d("BaseMapFragment", location.toString())
+                map.animateCamera(
+                    CameraUpdateFactory.newLatLngZoom(
+                        LatLng(
+                            location.latitude,
+                            location.longitude
+                        ), 16f
+                    ), 1000, null
+                )
+            }
+
+        fusedLocationClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            Looper.getMainLooper()
+        )
+
     }
+
+    private fun checkPermission() {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
+            return
+        }
+    }
+
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
