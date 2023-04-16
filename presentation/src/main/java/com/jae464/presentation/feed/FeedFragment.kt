2bfd167b635949f876129ea0cf4d19e7e530ebdf
이민재 <>
2023-04-16 12:50:54 +0900

    [FEAT] FeedFragment 에서 onTabSelectedListener 구현

--- presentation/src/main/java/com/jae464/presentation/feed/FeedFragment.kt
+++ presentation/src/main/java/com/jae464/presentation/feed/FeedFragment.kt
@@ -1,12 +1,14 @@
 package com.jae464.presentation.feed
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.jae464.presentation.R
 import com.jae464.presentation.base.BaseFragment
@@ -15,29 +17,60 @@ import com.jae464.presentation.databinding.FragmentFeedBinding
 
 class FeedFragment : BaseFragment<FragmentFeedBinding>(R.layout.fragment_feed) {
 
-    private val tabTitleArray = arrayOf(
-        "폴더별",
-        "카테고리별"
-    )
+    private val TAG = "FeedFragment"
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setTabLayout()
-        setAppBar()
+        initTabLayout()
+        initAppBar()
+        initListener()
     }
 
-    private fun setTabLayout() {
+    private fun initTabLayout() {
         val adapter = FeedStateAdapter(this)
         binding.viewPager.adapter = adapter
+        binding.tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                Log.d(TAG, tab?.position.toString())
+                when(tab?.position) {
+                    FOLDER_PAGE -> {
 
+                    }
+                    CATEGORY_PAGE -> {
+                        // VIEW MODE INFLATE
+                    }
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+
+        })
         TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab , position ->
             tab.text = tabTitleArray[position]
         }.attach()
+
     }
 
-    private fun setAppBar() {
+    private fun initAppBar() {
         val appBarConfiguration = AppBarConfiguration(findNavController().graph)
         binding.toolbarFeed.setupWithNavController(findNavController(), appBarConfiguration)
     }
 
+    private fun initListener() {
+
+    }
+
+    companion object {
+        private const val FOLDER_PAGE = 0
+        private const val CATEGORY_PAGE = 1
+
+        private val tabTitleArray = arrayOf("폴더별", "카테고리별")
+    }
 
 }
