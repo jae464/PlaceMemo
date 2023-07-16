5f6a207e29a6539610980d2b91856f15fdf3d722
이민재 <jae464@naver.com>
2023-07-16 23:09:52 +0900

    [FEAT] 메모 피드 페이지에서 앱바에 CoordinatorLayout 적용 및 정렬 기준 선택을 다이얼로그로 분리.

---
+++ presentation/src/main/java/com/jae464/presentation/common/FeedSettingDialog.kt
@@ -0,0 +1,31 @@
+package com.jae464.presentation.common
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.DialogFragment
+import com.jae464.presentation.R
+import com.jae464.presentation.databinding.DialogFeedSettingBinding
+
+class FeedSettingDialog : DialogFragment() {
+    private lateinit var binding: DialogFeedSettingBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.dialog_feed_setting, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+    }
+}
