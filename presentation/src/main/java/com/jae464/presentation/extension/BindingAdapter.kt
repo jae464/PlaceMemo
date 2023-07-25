579494cf194fa8bf3edeec5becc5e5f68f6332ea
이민재 <>
2023-07-26 00:46:19 +0900

    [FEAT] 유저 설정 저장을 위한 Data Store 라이브러리 추가. Hilt 로 의존성 주입 적용.

--- presentation/src/main/java/com/jae464/presentation/extension/BindingAdapter.kt
+++ presentation/src/main/java/com/jae464/presentation/extension/BindingAdapter.kt
@@ -5,7 +5,6 @@ import android.view.View
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.viewpager2.widget.ViewPager2
-import com.bumptech.glide.Glide
 
 @BindingAdapter("bindImage")
 fun bindImage(view: ImageView, filePath: String) {
